6e4ab123b2b9dd801c560c467bb37c9181a7ff27
Alexander <alexander.reznik.01@gmail.com>
2017-07-11 18:06:32 +0300

    Bugs fixed

--- DoubleExtensions/BinaryRepresentation.cs
+++ DoubleExtensions/BinaryRepresentation.cs
@@ -2,17 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DoubleExtensions
 {
+    [StructLayout(LayoutKind.Explicit)]
+    struct LongDoubleConverter
+    {
+        [FieldOffset(0)] public long LongValue;
+        [FieldOffset(0)] public double DoubleValue;
+    }
+
     public static class BinaryRepresentation
     {
-        public static string DoubleToIEEE754(double d)
+        public static string DoubleToIEEE754(this double d)
         {
             BitArray bits = new BitArray(BitConverter.GetBytes(d));
             return BitArrayToString(bits);
+        }
+
+        public static string DoubleToIEEE754Updated(this double d)
+        {
+            long l = GetLongFromDouble(d);
+            return GetBitsFromLong(l);
         }
 
         private static string BitArrayToString(BitArray ba)
@@ -21,6 +36,22 @@ namespace DoubleExtensions
             for (int i = ba.Length - 1; i >= 0; i--)
                 ans.Append(ba[i] ? '1' : '0');
             return ans.ToString();
+        }
+
+        private static long GetLongFromDouble(double d)
+        {
+            LongDoubleConverter c = new LongDoubleConverter();
+            c.DoubleValue = d;
+            return c.LongValue;
+        }
+
+        private static string GetBitsFromLong(long l)
+        {
+            long mask = 1;
+            StringBuilder answer = new StringBuilder();
+            for (int i = 63; i >= 0; i--)
+                answer.Append((l >> i) & mask);
+            return answer.ToString();
         }
     }
 }
--- Polynomial/Polynomial.cs
+++ Polynomial/Polynomial.cs
@@ -4,17 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Polynomial
 {
     public class Polynomial
     {
-        private double[] coefficients;
+        private readonly double[] coefficients;
+        private static readonly double eps = 0.0001;
+
+        private double[] Coefficients
+        {
+            get
+            {
+                if (coefficients == null) throw new ArgumentException();
+                return coefficients;
+            }
+        }
+
+        static Polynomial()
+        {
+            bool sucsess = false;//double.TryParse(ConfigurationManager.ConnectionStrings["epsilon"].ConnectionString, out eps);
+            if (!sucsess) eps = 0.001;
+        }
 
         public Polynomial(params double[] c)
         {
             if(c == null || c.Length == 0) throw new ArgumentException();
-            coefficients = new double[Array.FindLastIndex(c, d => d != 0) + 1];
+            coefficients = new double[Array.FindLastIndex(c, d => Math.Abs(d) >= eps) + 1];
             Array.Copy(c, coefficients, coefficients.Length);
         }
 
@@ -22,88 +39,87 @@ namespace Polynomial
 
         public int GetDegree()
         {
-            Check();
-            return coefficients.Length - 1;
+            return Coefficients.Length - 1;
         }
 
         public double GetValueInPoint(double x)
         {
             double ans = 0;
-            for (int i = 0; i < coefficients.Length; i++)
-                ans += coefficients[i] * Math.Pow(x, i);
+            for (int i = 0; i < Coefficients.Length; i++)
+                ans += Coefficients[i] * Math.Pow(x, i);
             return ans;
         }
 
         public override string ToString()
         {
-            Check();
             StringBuilder sb = new StringBuilder();
-            int i = Array.FindLastIndex(coefficients, d => d != 0);
+            int i = Array.FindLastIndex(Coefficients, d => Math.Abs(d) >= eps);
             if (i == -1) return "0";
-            sb.Append($"{coefficients[i]}x^{i--}");
+            sb.Append($"{Coefficients[i]}x^{i--}");
             for (; i >= 2; i--)
             {
-                if (coefficients[i] != 0) sb.Append($" + {coefficients[i]}x^{i}");
+                if (Math.Abs(Coefficients[i]) >= eps) sb.Append($" + {Coefficients[i]}x^{i}");
             }
-            if (coefficients[1] != 0) sb.Append($" + {coefficients[1]}x");
-            if (coefficients[0] != 0) sb.Append($" + {coefficients[0]}");
+            if (Math.Abs(Coefficients[1]) >= eps) sb.Append($" + {Coefficients[1]}x");
+            if (Math.Abs(Coefficients[0]) >= eps) sb.Append($" + {Coefficients[0]}");
 
             return sb.ToString();
         }
 
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            unchecked
+            {
+                int x = 0;
+                for (int i = 0; i < Coefficients.Length; i++)
+                    x ^= (int)Coefficients[i];
+                return x;
+            }
         }
 
         public override bool Equals(object obj)
         {
-            Check();
             if(obj == null || obj.GetType() != typeof(Polynomial)) return false;
-            IStructuralEquatable iArray = ((Polynomial)obj).coefficients;
-            return iArray.Equals(coefficients, StructuralComparisons.StructuralEqualityComparer);
+            IStructuralEquatable iArray = ((Polynomial)obj).Coefficients;
+            return iArray.Equals(Coefficients, StructuralComparisons.StructuralEqualityComparer);
         }
 
-        public static Polynomial operator +(Polynomial a, Polynomial b)
+        public static Polynomial operator +(Polynomial lhs, Polynomial rhs)
         {
-            int degree = Math.Max(a.GetDegree(), b.GetDegree());
+            CheckNullArguments(lhs, rhs);
+            int degree = Math.Max(lhs.GetDegree(), rhs.GetDegree());
             double[] c = new double[degree + 1];
             for (int i = 0; i <= degree; i++)
             {
-                c[i] += a.GetDegree() >= i ? a.coefficients[i] : 0;
-                c[i] += b.GetDegree() >= i ? b.coefficients[i] : 0;
+                c[i] += lhs.GetDegree() >= i ? lhs.Coefficients[i] : 0;
+                c[i] += rhs.GetDegree() >= i ? rhs.Coefficients[i] : 0;
             }
             return  new Polynomial(c);
         }
 
-        public static Polynomial operator -(Polynomial a, Polynomial b)
+        public static Polynomial operator -(Polynomial lhs, Polynomial rhs)
         {
-            int degree = Math.Max(a.GetDegree(), b.GetDegree());
-            double[] c = new double[degree + 1];
-            for (int i = 0; i <= degree; i++)
-            {
-                c[i] += a.GetDegree() >= i ? a.coefficients[i] : 0;
-                c[i] -= b.GetDegree() >= i ? b.coefficients[i] : 0;
-            }
-            return new Polynomial(c);
+            return lhs + (-1) * rhs;
         }
 
-        public static Polynomial operator *(Polynomial a, Polynomial b)
+        public static Polynomial operator *(Polynomial lhs, Polynomial rhs)
         {
-            int degree = a.GetDegree() + b.GetDegree();
+            CheckNullArguments(lhs, rhs);
+            int degree = lhs.GetDegree() + rhs.GetDegree();
             double[] c = new double[degree + 1];
-            for (int i = 0; i <= a.GetDegree(); i++)
-            for (int j = 0; j <= b.GetDegree(); j++)
-                c[i + j] += a.coefficients[i] * b.coefficients[j];
+            for (int i = 0; i <= lhs.GetDegree(); i++)
+            for (int j = 0; j <= rhs.GetDegree(); j++)
+                c[i + j] += lhs.Coefficients[i] * rhs.Coefficients[j];
             return new Polynomial(c);
         }
 
         public static Polynomial operator *(Polynomial a, double d)
         {
+            CheckNullArguments(a);
             int degree = a.GetDegree();
             double[] c = new double[degree + 1];
             for (int i = 0; i <= a.GetDegree(); i++)
-                c[i] = d * a.coefficients[i];
+                c[i] = d * a.Coefficients[i];
             return new Polynomial(c);
         }
 
@@ -112,19 +128,25 @@ namespace Polynomial
             return a * d;
         }
 
-        public static bool operator ==(Polynomial a, Polynomial b)
+        public static bool operator ==(Polynomial lhs, Polynomial rhs)
         {
-            return a.Equals(b);
+            if (ReferenceEquals(lhs, rhs)) return true;
+            if (ReferenceEquals(lhs, null)) return false;
+            return lhs.Equals(rhs);
         }
 
-        public static bool operator !=(Polynomial a, Polynomial b)
+        public static bool operator !=(Polynomial lhs, Polynomial rhs)
         {
-            return !a.Equals(b);
+            return lhs != rhs;
         }
 
-        private void Check()
+        private static void CheckNullArguments(Polynomial lhs, Polynomial rhs)
         {
-            if(coefficients == null) throw new ArgumentException();
+            if(ReferenceEquals(lhs, null) || ReferenceEquals(rhs, null)) throw new ArgumentNullException();
+        }
+        private static void CheckNullArguments(Polynomial a)
+        {
+            if (ReferenceEquals(a, null)) throw new ArgumentNullException();
         }
     }
 }
